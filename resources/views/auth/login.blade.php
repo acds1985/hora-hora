8f1d9911954258fb90e4b3c2e847eada1319bc6b
André Correia Dos Santos <andrecorreia0605@gmail.com>
2021-10-26 01:02:02 -0300

    Alteração da Logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,9 +2,10 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+               
+                <img src="{{ asset('img/logo-hora-hora.png') }}" alt="" class="w-150 h-40 fill-current text-gray-500">
             </a>
-        </x-slot>
+        </x-slot> 
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
