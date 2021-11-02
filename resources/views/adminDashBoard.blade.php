4f1e606403a6efeadaad75bdc1f8174e832d5d69
André Correia Dos Santos <andrecorreia0605@gmail.com>
2021-11-01 23:37:19 -0300

    Remove id in table admin

--- resources/views/adminDashBoard.blade.php
+++ resources/views/adminDashBoard.blade.php
@@ -6,94 +6,94 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 w-full">
             @if (session()->exists('message'))
                 <div class="flex flex-col justify-between w-full px-4 mb-3 py-4 text-{{ session()->get('color') }}-700 bg-{{ session()->get('color') }}-100 rounded"> {{session()->get('message')}}</div>
             @endif  
             
-                <div class="container">
-                    <div class="unit w-2-3"><div class="hero-callout">
-                        <table id="tablecert" class="display" style="width:100%">
-                                    @if (isset($data))
-                                    <thead class="bg-gray-50">
-                                        <tr>
-                                            <th>
-                                                ID
-                                            </th>
-                                            <th>
-                                                Nome
-                                            </th>
-                                            <th>
-                                                Tipo
-                                            </th>
-                                            <th>
-                                                Aluno
-                                            </th>
-                                            <th>
-                                                Horas
-                                            </th>
-                                            <th>
-                                                Adicionado em:
-                                            </th>
-                                            <th>
-                                                Status
-                                            </th>
-                                            <th>
-                                                Ações
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    @endif
-                                    <tbody>
-                                        @foreach($data as $cert)
-                                        <tr align="center">
-                                            <td>
-                                                {{ $cert->id }}
-                                            </td>
-                                            <td>
-                                               
-                                                  {{ $cert->titulo }}
+                <div class="w-full">
+                    <div class="unit w-2-3">
+                        <div class="hero-callout">
+                            <table id="tablecert" class="display">
+                                        @if (isset($data))
+                                        <thead class="bg-gray-50">
+                                            <tr class="w-auto">
+                                              
+                                                <th>
+                                                    Nome
+                                                </th>
+                                                <th>
+                                                    Tipo
+                                                </th>
+                                                <th>
+                                                    Aluno
+                                                </th>
+                                                <th>
+                                                    Horas
+                                                </th>
+                                                <th>
+                                                    Adicionado em:
+                                                </th>
+                                                <th>
+                                                    Status
+                                                </th>
+                                                <th >
+                                                    Ações
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        @endif
+                                        <tbody>
+                                            @foreach($data as $cert)
+                                            <tr align="center" class="w-1/2 md:w-full">
+                                            
+                                                <td>
                                                 
-                                            </td>
-                                            <td>
+                                                    {{ $cert->titulo }}
+                                                    
+                                                </td>
+                                                <td>
+                                                    
+                                                        {{ $cert->tipo }}
                                                 
-                                                    {{ $cert->tipo }}
-                                               
-                                            </td>
-                                            <td >
-                                           
-                                                    {{ $cert->student()->first()->name }}
+                                                </td>
+                                                <td >
+                                            
+                                                        {{ $cert->student()->first()->name }}
+                                                    
+                                                </td>
+                                                <td>
                                                 
-                                            </td>
-                                            <td>
-                                               
-                                                    {{ $cert->horas }}
-                                               
-                                            </td>
-                                            <td>
- 
-                                                    {{ date('d/m/Y', strtotime( $cert->created_at)) }}
-                                               
-                                            </td>
-                                            <td>
-                                                @if ($cert->status == 0)
-                                                    <label class="px-4 py-1 text-sm text-yellow-600 bg-yellow-200 rounded-full">Em Analise</label>
-                                                @elseif ($cert->status == 1)
-                                                    <label class="px-4 py-1 text-sm text-green-600 bg-green-200 rounded-full">Homologado</label>
-                                                @else
-                                                    <label class="px-4 py-1 text-sm text-red-600 bg-red-200 rounded-full">Não-Homologado</label>
-                                                @endif
-                                            </td>
-                                            <td align="center">
-                                                <a target="blank" href="{{ url("storage/$cert->path") }}" class="px-4 py-1 text-sm text-gray-600 bg-gray-200 rounded-full">Ver Aquivo</a>
-                                                <a href="{{ route('certificate.approve', ['id' => $cert->id]) }}" class="px-4 py-1 text-sm text-blue-600 bg-blue-200 rounded-full">Homologar</a>
-                                                <a href="{{ route('certificate.notapprove', ['id' => $cert->id]) }}" class="px-4 py-1 text-sm text-red-400 bg-red-200 rounded-full">Não Homologar</a>
-                                            </td>
-                                        </tr>   
-                                @endforeach
-                            </tbody>
-                        </table>
-                      </div>
+                                                        {{ $cert->horas }}
+                                                
+                                                </td>
+                                                <td>
+    
+                                                        {{ date('d/m/Y', strtotime( $cert->created_at)) }}
+                                                
+                                                </td>
+                                                <td>
+                                                    @if ($cert->status == 0)
+                                                        <label class="px-4 py-1 text-sm text-yellow-600 bg-yellow-200 rounded-full">Em Analise</label>
+                                                    @elseif ($cert->status == 1)
+                                                        <label class="px-4 py-1 text-sm text-green-600 bg-green-200 rounded-full">Homologado</label>
+                                                    @else
+                                                        <label class="px-4 py-1 text-sm text-red-600 bg-red-200 rounded-full">Não-Homologado</label>
+                                                    @endif
+                                                </td>
+                                                <td align="center">
+                                                    
+
+                                                        <a target="blank" href="{{ url("storage/$cert->path") }}" class="px-4 py-1 text-sm text-gray-600 bg-gray-200 rounded-full">Ver Aquivo</a>
+                                                        <a href="{{ route('certificate.approve', ['id' => $cert->id]) }}" class="px-4 py-1 text-sm text-blue-600 bg-blue-200 rounded-full">Homologar</a>
+                                                        <a href="{{ route('certificate.notapprove', ['id' => $cert->id]) }}" class="px-4 py-1 text-sm text-red-400 bg-red-200 rounded-full">Não Homologar</a>
+                                                    
+                                                </td>
+                                            </tr>   
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>    
                 </div>
         </div>
